3eb5f5073ee22f2c4db0b4619c9dec128c479a32
AnanJaser <ananjaser@gmail.com>
2019-04-17 11:04:27 +0400

    board: add kernel page size

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,7 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 # Kernel
 TARGET_PREBUILT_KERNEL := device/samsung/j5y17lte/kernel
 BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/j5y17lte/dt.img
 
 # File systems
